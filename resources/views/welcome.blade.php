08ca65af952354b502fcc1b56c19ee884b1cebb3
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2023-01-02 07:20:54 +0700

    Integrated to laravel 8.83

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('maincss/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard-styles.css') }}">
+</head>
+<body>
+
+    <div class="testing-title">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tempora nisi est sequi similique, quos eligendi molestias alias commodi dolores et quo repellat, quia architecto labore eaque possimus nam perspiciatis?
+    </div>
+
+    <div class="heading w-[auto] block ">
+        <h1 class="text-3xl font-bold  text-center  align-middle justify-center pt-96 ">
+            Hello, This projects build with Laravel 8 and Tailwindcss
+        </h1>
+    </div>
+</body>
+</html>
